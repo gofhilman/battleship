f8bc056dcd156d2ee10d4b58a4e9fb4afe9ec3f3
Hilman Fikry <hilman12fikry@gmail.com>
2025-06-02 15:11:10 +0700

    refactor: move create players subscription
    Move it in the UI script (index.js) to make the codebase cleaner

--- src/game-control.js
+++ src/game-control.js
@@ -1,12 +1,9 @@
-import PubSub from "pubsub-js";
 import Player from "./player";
-import { OPPONENT } from "./pubsub-msg";
 import { resolveSubscription } from "./event-handlers";
 
 class GameControl {
   constructor() {
     this.players = [];
-    PubSub.subscribe(OPPONENT.TYPE, this.createPlayers.bind(this));
   }
 
   createPlayers(_, opponentType) {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import "./styles.css";
 import "../node_modules/modern-normalize/modern-normalize.css";
 import { displayGameSetting } from "./dialogs";
 import GameControl from "./game-control";
+import PubSub from "pubsub-js";
+import { OPPONENT } from "./pubsub-msg";
 
 const gameControl = new GameControl();
+PubSub.subscribe(OPPONENT.TYPE, gameControl.createPlayers.bind(gameControl));
 displayGameSetting(gameControl.players);
