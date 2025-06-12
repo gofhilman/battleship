dddabd24aae2cc75f288ddc43f227cf4aa336699
Hilman Fikry <hilman12fikry@gmail.com>
2025-06-12 10:40:56 +0700

    refactor: move modern-normalize import to css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./styles.css";
-import "../node_modules/modern-normalize/modern-normalize.css";
 import "./global-subscription";
 import { displayGameSetting } from "./dialogs";
 import gameControl from "./game-control";
